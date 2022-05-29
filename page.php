230047d328f1afe7d8e62e6e2bd382f09dcdc53d
cadentic <55697529+cadentic@users.noreply.github.com>
2022-05-29 19:50:22 +0530

    page.php updated

---
+++ page.php
@@ -0,0 +1,356 @@
+<?php get_header();
+
+while(have_posts()){
+
+the_post();  ?>
+
+	
+		<section id="topextsec">
+		
+			<div class="leftext col-lg-4 col-md-4 col-sm-4">
+					 <div class="imgext1">
+					 <img src="<?php echo THEME_IMG_PATH; ?>/img1.png" alt="" />
+					 </div>
+					 <div class="imgextt2">
+					  <img src="<?php echo THEME_IMG_PATH; ?>/img2.png" alt="" />
+					 </div>
+					
+			</div>
+			
+			
+			
+			
+			<div class="rightext col-lg-7 col-md-7 col-sm-7" >
+					<h2>Would you like to change the default language?</h2>
+					<a href="">Select Now</a>
+					<a href="">No Thanks</a>
+					
+			</div>
+			<div class="rightico col-lg-1 col-md-1 col-sm-1">
+			<i class="clo fa fa-close"></i>
+			</div>
+			
+			<hr />
+			
+			<nav class="extnav">
+			
+			 <ul>
+			 <li><a href="#topsolu">Top Solution</a></li>
+			 <li class="bdr" ><a href="">Partners Info</a></li>
+			 <li class="bdr" ><a href="">Whats New</a></li>
+			 <li class="bdr" ><a href="">Happy Clients</a></li>
+			 <li class="bdr" ><a href="">Other Division</a></li>
+			 
+			 </ul>
+			
+			</nav>
+		
+		</section>
+		
+		
+		<section id="vertmenu-sec" >
+			<div class="vert-menu col-lg-2 col-md-2 col-sm-2 col-xs-2">
+				<div class="humberger">
+						 <div class=" barss" >	
+								<div class="barss1"> <hr /> </div>
+								<div class="barss2"> <hr /> </div>
+								<div class="barss3"> <hr /> </div>
+							
+								
+						</div>	
+						
+						
+				<div class="tab3">
+				  <div data-child="false" class="tab__header left-side">
+					<div class="tab__header-1 tab__header--active">Dtabase Administration</div>
+					<div class="tab__header-2">Cloude</div>
+					<div class="tab__header-3">Migration</div>
+					<div class="tab__header-5">Directors info</div>
+					<div class="tab__header-5">Nature & Mode Of Work</div>
+					<div class="tab__header-6">Hardwar and Networking Component</div>
+					<div class="tab__header-7">Staffing</div>
+					<div class="tab__header-8">Corporate Training on Oracle</div>
+					<div class="tab__header-9">Corporate Training on Redhat</div>
+				  </div>
+		  
+
+		  
+		  
+		  <div class="tab__content">
+		  
+			<div class="tab__content-1 tab__content--active">
+			  <div class="tab">
+				<div data-child="true" class="tab__header middle-side">
+				  <div class="tab__header-1 tab__header--active">E-Business suit</div>
+				  <div class="tab__header-2">Single Sign in</div>
+				  <div class="tab__header-3">Golden Gate</div>
+				  <div class="tab__header-4">Open Source</div>
+				</div>
+				
+				
+				<div class="tab__content right-side">
+				  <div class="tab__content-1 tab__content--active">Sub Tab Content 1</div>
+				  <div class="tab__content-2">Sub Tab Content 2</div>
+				  <div class="tab__content-3">Sub Tab Content 3</div>
+				  <div class="tab__content-4">Sub Tab Content 4</div>
+				</div>
+			  </div>
+			</div>
+			
+			
+			
+			<div class="tab__content-2">
+			  <div class="tab">
+				<div data-child="true" class="tab__header middle-side">
+				  <div class="tab__header-1 tab__header--active">Sub Tab 2 1</div>
+				  <div class="tab__header-2">Sub Tab 2 2</div>
+				  <div class="tab__header-3">Sub Tab 2 3</div>
+				  <div class="tab__header-4">Sub Tab 2 4</div>
+				</div>
+				
+				
+				<div class="tab__content right-side">
+				  <div class="tab__content-1 tab__content--active">Sub Tab Content 2 1</div>
+				  <div class="tab__content-2">Sub Tab Content 2 2</div>
+				  <div class="tab__content-3">Sub Tab Content 2 3</div>
+				  <div class="tab__content-4">Sub Tab Content 2 4</div>
+				</div>
+			  </div>	
+			 
+			</div>
+			
+			
+			
+			
+			<div class="tab__content-3">
+			  <div class="tab">
+				<div data-child="true" class="tab__header middle-side">
+				  <div class="tab__header-1 tab__header--active">Sub Tab 3 1</div>
+				  <div class="tab__header-2">Sub Tab 3 2</div>
+				  <div class="tab__header-3">Sub Tab 3 3</div>
+				</div>
+				<div class="tab__content right-side">
+				  <div class="tab__content-1 tab__content--active">Sub Tab Content 3 1</div>
+				  <div class="tab__content-2">Sub Tab Content 3 2</div>
+				  <div class="tab__content-3">Sub Tab Content 3 3</div>
+				</div>
+			  </div>
+			</div>
+			
+			
+			<div class="tab__content-4">
+			  <div class="tab">
+				<div data-child="true" class="tab__header middle-side">
+				  <div class="tab__header-1 tab__header--active">Sub Tab 4 1</div>
+				  <div class="tab__header-2">Sub Tab 4 2</div>
+				  <div class="tab__header-3">Sub Tab 4 3</div>
+				</div>
+				<div class="tab__content right-side">
+				  <div class="tab__content-1 tab__content--active">Sub Tab Content 4 1</div>
+				  <div class="tab__content-2">Sub Tab Content 4 2</div>
+				  <div class="tab__content-3">Sub Tab Content 4 3</div>
+				</div>
+			  </div>
+			</div>
+			
+		  </div>
+
+</div>		
+						
+
+								
+						
+
+
+						  
+
+
+							
+						
+						
+					  
+						
+						
+						
+
+				
+				</div>
+			
+			</div>
+			
+			
+			<div class="vert-search col-lg-7 col-md-7 col-sm-7 col-xs-7">
+			
+					<div class="search-animated">
+					<form action="">
+					<input type="text" class="input" placeholder="What are you looking for?">
+					<div class="sub-ico">
+					<input type="submit" value="" class="close-btn">
+					<i class="fa fa-search"></i>
+					
+					</div>
+					</form>
+					</div>
+ 
+			
+			
+			</div>
+			<div class="vert-logo col-lg-3 col-md-3 col-sm-3 col-xs-3">
+				<div class="logo-div">
+				<a href=""><img src="<?php echo THEME_IMG_PATH; ?>/logo.png" alt="" /></a>
+				</div>
+			</div>
+		
+		
+		</section>
+	
+	  <!-- Tab panes -->
+	  
+	  
+	  
+
+	
+	
+
+	<section class="slider_sec" >
+	
+			<div class="owl-carousel owl-theme banner_slide">
+			
+			<div class="itm"><img src="<?php echo THEME_IMG_PATH; ?>/5042_banner_1.jpg" alt="" /> 
+				<div class="h1pox" style="position:absolute;left:100px;top:80px;color:#fff" >
+				<h1 style="margin-left:15px;">Zero-downtime migration using golden gate</h1>
+				<img src="images/Untitled-1.png" alt="" />
+				</div>
+			</div>
+			<div class="itm"><img src="<?php echo THEME_IMG_PATH; ?>/5042_banner_2.jpg" alt="" />
+			<div class="h1pox" style="padding:10px 30px;position:absolute;left:100px;top:80px;color:#fff" >
+				<h1>On-premise to cloud migration</h1>
+				<a href="">Visit For More Info</a> 
+				<a href="">Become Our Client</a>
+			</div>
+			
+			
+			</div>
+			<div class="itm"><img src="<?php echo THEME_IMG_PATH; ?>/5042_banner_3.jpg" alt="" />
+			<div class="h1pox" style="position:absolute;left:100px;top:60px;color:#fff" >
+				<h1 style="text-align:center;">E-Business Suit database Administration</h1>
+				<a href="" style="margin-left:65px; background:transparent;color:#22ADFE; border:2px solid #22ADFE; font-size:30px;">Visit For More Info</a> 
+				<a href="" style="margin-left:65px; background:transparent;color:#22ADFE; border:2px solid #22ADFE; font-size:30px;">Become Our Client</a>
+				
+			</div>
+			
+			</div>
+			<div class="itm"><img src="im<?php echo THEME_IMG_PATH; ?>/5042_banner_4.jpg" alt="" />
+			<div class="h1pox" style="padding:10px 30px;position:absolute;right:100px;top:80px;color:#fff" >
+				<h1 style="width:600px">Upgradstion and Migration of different database</h1>
+				<a href="" style="background:#4893BB;color:#fff; border:2px solid #000; font-size:30px;">Migration Methodology</a> 
+				<a href="" style="background:#4893BB;color:#fff; border:2px solid #000; font-size:30px;">Become Our Client</a>
+				
+				
+			</div>
+			
+			
+			</div>
+			<div class="itm"><img src="<?php echo THEME_IMG_PATH; ?>/5042_banner_5.jpg" alt="" />
+			<div class="h1pox" style="padding:10px 30px;position:absolute;right:100px;top:60px;color:#fff" >
+			<img src="<?php echo THEME_IMG_PATH; ?>/Untitled-1.png" alt="" />
+				<h1 style="width:600px; font-size:48px; text-align:center; margin-top:0px; margin-left:15px;">IBM business Partners AWS Architect <br /> Oracle Sliver Partners </h1>
+				
+				
+			</div>
+			
+			</div>
+	
+		
+			</div>
+										
+	
+	
+	</section>
+	
+	
+	<nav class="navbar navbar-inverse" style="background:#fff; padding:0px 130px; border-color:#fff;">
+		  <div class="container-fluid">
+			
+			<ul class="nav navbar-nav">
+			  <li class="active"><a href="#home">Home</a></li>
+			  <li><a href="#pg1" >Replace me with section 1</a></li>
+			  <li><a href="#pg2">Replace me with section 2</a></li>
+			  <li><a href="#pg3">Replace me with section  3</a></li>
+			  <li><a href="#pg4">Replace me with section 4</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+			
+			   <li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-envelope"></span> Contact
+					<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+					  <li><a href="#">Call Now</a></li>
+					  <li><a href="#">Page 1-2</a></li>
+					  <li><a href="#">Page 1-3</a></li>
+					</ul>
+				  </li>
+			
+			</ul>
+		  </div>
+		</nav>
+	
+	
+				<section class="content" id="home" style="background:#E6F5FF; ">
+					<h3><?php the_title(); ?></h3>
+					
+					<ul type="disc" style="list-style-type: disc;" >
+						<li style="font-size:20px; font-weight:bold;">INTRODUCTION</li>
+					</ul>
+					<p>
+					Ndedge Informatics Private Limited is a leading global multi-skilled IT solution company with competent workforce and strong presence in India. With several years of expertise, quality delivery of service and quality integrated methodologies; Ndedge Informatics Private Limited have been able to carve out a niche for themselves as one of the most notable trailblazers in the IT Industry in India. <br />
+					Ndedge Informatics Private Limited uses an extremely competent global delivery method in providing quality-based solutions and professional services to its esteemed clients. <br />
+					 Our strongest advantage is our excellent pool of our skilled workforce; our skilled resource is recruited from the finest the industry can boast of in India. We can boast of a vibrant skilled resource that provides a vibrant combination of technological skills and strategic IT consultancy services.
+					</p>
+				
+				
+				
+				
+				
+				
+				</section>
+				
+				<div class="hg content" id="pg1" style="height:500px; transition:2s; font-size:40px;background:#E6F5FF;
+				">
+				<h3>Ndedge Informatics Private Limited offers</h3>
+					<?php the_content(); ?>
+					
+					<h4 style="margin-bottom:30px;">Ndedge Informatics Private Limited has a solid client base that spans across several industries; in our focus market segment we have accumulated an enviable list of clients because of our customer centric approach. </h4>
+				
+				
+				</div>
+				<div class="hg content" id="pg2" style="height:500px; transition:2s; font-size:40px;background:#E6F5FF;
+				">
+				
+					
+					
+					<h4>Ndedge Informatics Private Limited competitive advantage and source of value to our esteemed client stems not only from our expertise in rendering IT professional services, but from several aspect of our business model which consist of </h4>
+					
+					
+					<ul type="disc" style="list-style-type: disc;" >
+						<li>technology and business competency centers, </li>
+						<li>rigorous programme and project management, </li>
+						<li>state of the art computing and communication infrastructure, client-centric approach account management,</li>
+						<li>Innovative offshore development approach and business continuity preparedness.We remain focus on our prime objective which is delivering high quality solutions to our clients </li>
+						
+					</ul>
+					
+				
+				
+				</div>
+				<div class="hg" id="pg3" style="height:500px; transition:2s;font-size:40px;
+				text-align:center;padding:100px;">Page 3</div>
+				<div class="hg" id="pg4" style="height:500px; transition:2s;font-size:40px;
+				text-align:center;padding:100px;">Page 4</div>
+		
+	
+		
+
+<?php }
+    get_footer();
+?>
